acd64cfe98fcd97bb4441d9c5378f5479f52dde7
Awadhesh Gupta <awadheshg798@gmail.com>
2025-05-03 16:32:15 +0530

    page.tsx change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -48,7 +48,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index:number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
